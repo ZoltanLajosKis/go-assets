41f9fc9dbe6fab73c2b4d1c448dc5cf1af10cda1
Zoltán Lajos Kis <zoltan.lajos.kis@gmail.com>
2017-08-16 06:56:46 +0200

    Refactor checksums

--- checksum.go
+++ checksum.go
@@ -43,48 +43,40 @@ func verifyChecksum(chksum *Checksum, data []byte) error {
 
 	switch chksum.Algo {
 	case ChecksumMD5:
-		return verifyChecksumMD5(chksum.Value, data)
+		return compare(chksum.Value, calcMD5(data))
 	case ChecksumSHA1:
-		return verifyChecksumSHA1(chksum.Value, data)
+		return compare(chksum.Value, calcSHA1(data))
 	case ChecksumSHA256:
-		return verifyChecksumSHA256(chksum.Value, data)
+		return compare(chksum.Value, calcSHA256(data))
 	case ChecksumSHA512:
-		return verifyChecksumSHA512(chksum.Value, data)
+		return compare(chksum.Value, calcSHA512(data))
 	default:
 		return ErrChecksumUnknown
 	}
 }
 
-func verifyChecksumMD5(value string, data []byte) error {
+func calcMD5(data []byte) []byte {
 	h := md5.New()
 	h.Write(data)
-	if value != hex.EncodeToString(h.Sum(nil)) {
-		return ErrChecksumMismatch
-	}
-
-	return nil
+	return h.Sum(nil)
 }
 
-func verifyChecksumSHA1(value string, data []byte) error {
+func calcSHA1(data []byte) []byte {
 	checksum := sha1.Sum(data)
-	if value != hex.EncodeToString(checksum[:]) {
-		return ErrChecksumMismatch
-	}
-
-	return nil
+	return checksum[:]
 }
 
-func verifyChecksumSHA256(value string, data []byte) error {
+func calcSHA256(data []byte) []byte {
 	checksum := sha256.Sum256(data)
-	if value != hex.EncodeToString(checksum[:]) {
-		return ErrChecksumMismatch
-	}
-
-	return nil
+	return checksum[:]
 }
 
-func verifyChecksumSHA512(value string, data []byte) error {
+func calcSHA512(data []byte) []byte {
 	checksum := sha512.Sum512(data)
+	return checksum[:]
+}
+
+func compare(value string, checksum []byte) error {
 	if value != hex.EncodeToString(checksum[:]) {
 		return ErrChecksumMismatch
 	}
